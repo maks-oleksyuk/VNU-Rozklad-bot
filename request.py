790b3da36164dd9f334c54cf1a6bcb029cb91876
maks-oleksyuk <maksym.oleksyuk@gmail.com>
2022-02-03 23:38:14 +0200

    03.02

--- request.py
+++ request.py
@@ -37,6 +37,23 @@ async def getChair():
             "http://194.44.187.20/cgi-bin/timetable_export.cgi", params=payload
         )
         text = json.loads(r.text)
+        index = []
+        for d in range(len(text["psrozklad_export"]["departments"]) - 1, -1, -1):
+            for g in range(
+                len(text["psrozklad_export"]["departments"][d]["objects"]) - 1, -1, -1
+            ):
+                n = text["psrozklad_export"]["departments"][d]["objects"][g]["name"]
+                if (
+                    n.find("Інфекціоніст") != -1
+                    or n.find("Онколог") != -1
+                    or n.find("Психіатр") != -1
+                    or n.find("Фтизіатр") != -1
+                    or n.find("Резерв") != -1
+                    or n.find("Вакансія") != -1
+                ):
+                    index.append([d, g])
+        for i in index:
+            del text["psrozklad_export"]["departments"][i[0]]["objects"][i[1]]
         if text["psrozklad_export"]["code"] == "0":
             with open("json/chair.json", "w+") as f:
                 json.dump(text, f, sort_keys=True, indent=2, ensure_ascii=False)
