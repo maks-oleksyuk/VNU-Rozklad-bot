ecc754bfb9fa4a866136c4b5178cdefd17ec1a64
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2023-01-17 19:21:29 +0200

    - update code create users table

--- src/database/db_init.py
+++ src/database/db_init.py
@@ -1,7 +1,9 @@
 from os import getenv
 
 import mariadb
-from sqlalchemy import inspect, create_engine
+from sqlalchemy import create_engine, inspect, MetaData, Table, Column
+from sqlalchemy.dialects.mysql import BOOLEAN, BIGINT, VARCHAR, TIMESTAMP
+from sqlalchemy.sql import func
 
 engine = create_engine(
     'mariadb+mariadbconnector://{}:{}@db:3306/{}'.format(
@@ -11,6 +13,7 @@ engine = create_engine(
     )
 )
 inspector = inspect(engine)
+meta = MetaData()
 
 con = mariadb.connect(
     host='db',
@@ -22,12 +25,25 @@ cur = con.cursor()
 
 
 async def db_init():
-    if not inspector.has_table('users'):
-        await create_table_users()
+    if not inspector.has_table('users_t5'):
+        Table(
+            'users_t5', meta,
+            Column('uid', BIGINT(unsigned=True), unique=True, primary_key=True,
+                   comment='The Telegram user ID.'),
+            Column('name', VARCHAR(255),
+                   comment='The name of this user.'),
+            Column('username', VARCHAR(255),
+                   comment='The Telegram pseudonym of this user.'),
+            Column('status', BOOLEAN, nullable=False, default=True,
+                   comment='Whether the user is active or blocked.'),
+            Column('login', TIMESTAMP, nullable=False, default=func.utcnow(),
+                   comment='The time that the user last logged in.')
+        )
     if not inspector.has_table('users_data'):
         await create_table_users_data()
     if not inspector.has_table('timetable'):
         await create_table_timetable()
+    meta.create_all(engine)
 
 
 async def db_close():
@@ -35,33 +51,17 @@ async def db_close():
     con.close()
 
 
-async def create_table_users():
-    try:
-        cur.execute("""
-            CREATE TABLE users (
-                uid      serial comment 'The Telegram user ID.',
-                name     char(255) comment 'The name of this user.',
-                username char(255) comment 'The Telegram pseudonym of this user.',
-                status   boolean NOT NULL DEFAULT TRUE comment 'Whether the user is active or blocked.',
-                login    timestamp DEFAULT CURRENT_TIMESTAMP comment 'The time that the user last logged in.'
-            );
-        """)
-        con.commit()
-    except Exception as e:
-        print(f'DB Error: {e}')
-
-
 async def create_table_users_data():
     try:
         cur.execute("""
-            CREATE TABLE users_data (
-                uid    serial comment 'The Telegram user ID.',
-                d_id   integer  NOT NULL comment 'The Data ID.',
-                d_type char(65) NOT NULL comment 'Type of user in the system.',
-                d_name char(65) NOT NULL comment 'The name of the received data.',
-                d_date date     NOT NULL comment 'The Last date of requested data.'
-            );
-        """)
+        CREATE TABLE users_data (
+            uid    serial comment 'The Telegram user ID.',
+            d_id   integer  NOT NULL comment 'The Data ID.',
+            d_type char(65) NOT NULL comment 'Type of user in the system.',
+            d_name char(65) NOT NULL comment 'The name of the received data.',
+            d_date date     NOT NULL comment 'The Last date of requested data.'
+        );
+    """)
         con.commit()
     except Exception as e:
         print(f'DB Error: {e}')
@@ -70,11 +70,11 @@ async def create_table_users_data():
 async def create_table_timetable():
     try:
         cur.execute("""
-            create table timetable (
-                id   smallint unsigned not null comment 'The timetable data ID.',
-                mode varchar(255)         not null comment 'The data mode.'
-            );
-        """)
+        create table timetable (
+            id   smallint unsigned not null comment 'The timetable data ID.',
+            mode varchar(255)         not null comment 'The data mode.'
+        );
+    """)
         con.commit()
     except Exception as e:
         print(f'DB Error: {e}')
