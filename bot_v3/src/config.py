0ba3bfe5876bc32a72dda2c2ac495777ebc7a56b
Maks Oleksyuk <maksym.oleksyuk@gmail.com>
2023-03-01 21:28:50 +0200

    - add log config

--- bot_v3/src/config.py
+++ bot_v3/src/config.py
@@ -1,27 +1,19 @@
-import logging
+import logging.config
 from os import getenv
 
+import yaml
 from aiogram import Bot, Dispatcher
-from logging.handlers import RotatingFileHandler
 
-# Telegram bot API token from @BotFather
-BOT_TOKEN = getenv('BOT_TOKEN', default='')
-
-# Admin user ID
+# Admin user ID.
 ADMIN_ID = int(getenv('ADMIN_ID', default=''))
 
-# Logging settings
-LOG_LEVEL = getenv('LOG_LEVEL', default='INFO')
-logging.basicConfig(level=LOG_LEVEL)
-logger = logging.getLogger(__name__)
+# Loading the logger configuration from a file.
+with open('logs/log_conf.yml', 'r') as f:
+    log_config = yaml.safe_load(f.read())
+logging.config.dictConfig(log_config)
+logger = logging.getLogger('bot')
 
-# handler = RotatingFileHandler('bot.log', maxBytes=100000, backupCount=3)
-# formatter = logging.Formatter(
-#     '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
-
-# Initialize bot and dispatcher
-bot = Bot(token=BOT_TOKEN, validate_token=True,
+# Initialize bot and dispatcher.
+bot = Bot(token=getenv('BOT_TOKEN', default=''), validate_token=True,
           parse_mode='MarkdownV2', disable_web_page_preview=True)
 dp = Dispatcher(bot)
